fff7287580982f118a8f5b3da8ec66833198e15c
Jagadeeshftw <jagadeesh26062002@gmail.com>
2024-06-15 02:10:10 +0530

    Adding styles for carousel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import BootstrapClient from "../../components/BootstrapClient";
+import "animate.css";
 import "./globals.scss";
 import NavbarItem from "../../components/Navbars/Navbar";
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,8 @@
+import Carousel from "../../components/Carousel/Carousel";
 export default function Home() {
-  return <main></main>;
+  return (
+    <main>
+      <Carousel />
+    </main>
+  );
 }
